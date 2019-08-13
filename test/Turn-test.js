1d6aff5a1c69a8b53ba7b012d2fb2b9d33566e55
dawnlunacy <rudd.lacy@gmail.com>
2019-08-13 00:21:09 -0600

    Add turn class based on turn-test.js and add more test to turn-test.js

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -28,14 +28,14 @@ describe('Turn', function() {
   });
 
   it('should take an argument for a card representing the current card in play', function () {
-    let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'sea otter');
+    let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'elephant');
     const turn = new Turn('pug', card);
-    expect(turn.card).to.eql(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'sea otter')
+    expect(turn.card).to.eql(card)
   });
 
   describe('returnGuess', function() {
     it('should return the guess', function() {
-      let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'sea otter');
+      let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'elephant');
       const turn = new Turn('pug', card);
       expect(turn.returnGuess()).to.equal('pug');
     })
@@ -43,23 +43,36 @@ describe('Turn', function() {
 
   describe('returnCard', function() {
     it('should return the card', function() {
-      let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'sea otter');
+      let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'elephant');
       const turn = new Turn('pug', card);
-      expect(turn.returnCard()).to.eql(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'sea otter');
+      expect(turn.returnCard()).to.eql(card);
     });
   });
 
   describe('evaluateGuess', function() {
-    it('should return true if the guess is correct and false if the guess is wrong', function() {
-      let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'sea otter');
+    it('should return false if the guess is incorrect', function() {
+      let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'elephant');
       const turn = new Turn('pug', card);
-      expect(turn.returnCard()).to.equal(false);
+      expect(turn.evaluateGuess()).to.equal(false);
     });
+
+    it('should return true if the guess is correct', function() {
+      let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'elephant');
+      const turn = new Turn('elephant', card);
+      expect(turn.evaluateGuess()).to.equal(true);
+    });
+
   });
 
   describe('giveFeedBack', function() {
-    it('should return "incorrect!" if the guess is wrong and "correct!" if the guess is right' , function() {
-      let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'sea otter');
+    it('should return "correct!" if the guess is right' , function() {
+      let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'elephant');
+      const turn = new Turn('elephant', card);
+      expect(turn.giveFeedBack()).to.eql('correct!');
+    });
+      
+    it('should return "incorrect!" if the guess is wrong' , function() {
+      let card = new Card(1, 'What is Tyler\'s favorite animal', ['sea otter', 'elephant', 'pug', 'cat'], 'elephant');
       const turn = new Turn('pug', card);
       expect(turn.giveFeedBack()).to.eql('incorrect!');
     });
